e5086cf293546a59242ee91c1b966c3d4e9a7feb
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-01-22 21:15:46 +0700

    feat: finish 03-files-in-folder

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,17 @@
+const fs = require('node:fs');
+const path = require('node:path');
+const relativePath = path.join(__dirname, '/secret-folder');
+const { stdin, stdout } = require('node:process');
+// {withFileTypes: true},
+fs.readdir(relativePath, (err, files) => {
+  files.forEach((file) => {
+    console.log(file);
+    const filePath = path.join(relativePath, file);
+    // console.log(filePath);
+    fs.stat(filePath, (err, stat) => {
+      if (stat.isFile()) {
+        console.log(path.parse(file).name,'-',path.extname(file).slice(1),'-',stat.size,'Byte');
+      }
+    });
+  });
+});

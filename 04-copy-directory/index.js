fb3eac6abcbf7ae0fa065b216dd4279e48e607d4
sergkrav.social@gmail.com <sergkrav.social@gmail.com>
2024-01-23 20:07:48 +0700

    fix: fix 04-copy-directory update, rewrite in async

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,23 +1,39 @@
-const fs = require('node:fs');
 const path = require('node:path');
 const relativePath = path.join(__dirname, '/files');
 const copyRelativePath = path.join(__dirname, '/files-copy');
-const { stdin, stdout } = require('node:process');
-fs.mkdir(copyRelativePath, (err) => {
-  console.log('Dir coppied');
-  fs.readdir(relativePath, (err, files) => {
-    files.forEach((file) => {
-      // console.log(file);
-      fs.copyFile(path.join(relativePath, file), path.join(copyRelativePath, file), (err) => {
+const fsPromises = require('node:fs/promises');
+
+(async () => {
+  try {
+    await fsPromises.mkdir(copyRelativePath, { recursive: true }, (err) => {
+      if (err) {
+        console.log("Error Found:", err);
+      }         
+    });
+    const copyFiles = await fsPromises.readdir(copyRelativePath, (err) => {
+      if (err) {
+        console.log("Error Found:", err);
+      } 
+    });
+    for (let i = 0; i < copyFiles.length; i += 1) {
+      await fsPromises.rm(path.join(copyRelativePath, copyFiles[i]),{ recursive: true }, { force: true }, (err) => {
         if (err) {
           console.log("Error Found:", err);
-        } else {
-          console.log('File coppied',file);
-        }
+        } 
       });
+    }
+
+    
+    const files = await fsPromises.readdir(relativePath, (err) => {
+      if (err) {
+        console.log("Error Found:", err);
+      } 
     });
-  });
-});
-// fs.readdir(copyRelativePath, (err, filesCopy) => {
-//   console.log(copyRelativePath, filesCopy);
-// });
+    for (let i = 0; i < files.length; i += 1) {
+      await fsPromises.copyFile(path.join(relativePath, files[i]),path.join(copyRelativePath, files[i]));
+    }
+  }
+  catch (err) {
+    console.error(err.message);
+  }
+})();
